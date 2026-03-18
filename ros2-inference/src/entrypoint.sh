7affd14bd421ad982c81a7deb189b2db2fe43936
Luis Arizmendi <luis.ariz@gmail.com>
2026-03-18 11:48:20 +0100

    fix entrypoint

--- ros2-inference/src/entrypoint.sh
+++ ros2-inference/src/entrypoint.sh
@@ -3,7 +3,8 @@ set -e
 
 export ROS_HOME=/tmp/ros_home
 
-source /usr/lib64/ros2-kilted/setup.bash
+# Source ROS2
+source /opt/ros/kilted/setup.bash
 source /ros2_ws/install/setup.bash
 
 exec ros2 run inference_node inference_node
